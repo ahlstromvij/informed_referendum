a383abe9b57665303fb27053db75bb23593daa3a
Kristoffer Ahlstrom-Vij <ahlstromvij@Kristoffers-MacBook-Pro.local>
2023-03-21 22:44:26 +0000

    Add diagnostics, p values and predicted probabilities

--- R/04_models.R
+++ R/04_models.R
@@ -2,6 +2,7 @@ set.seed(1)
 
 library(tidyverse)
 library(nnet)
+library(performance)
 library(RColorBrewer)
 
 compl_data <- readRDS("data/bes_preprocessed_scales_prop.rds")
@@ -23,6 +24,9 @@ sample_leave <- sum(compl_data$wt_vote[compl_data$eu_vote=="leave"])
 sample_wt_remain <- sample_remain/(sample_remain + sample_leave)
 sample_wt_remain
 
+# Set reference level
+compl_data$eu_vote <- relevel(compl_data$eu_vote, ref = "no_vote")
+
 # PARTY ID MODEL
 m_partisan <- multinom(eu_vote ~ 
                         know_binary +
@@ -38,6 +42,60 @@ m_partisan <- multinom(eu_vote ~
                       data = compl_data, 
                       Hess = TRUE,
                       weights = prop_score)
+summary(m_partisan)
+# moving from uninformed to informed is associated with a with an increase in the log odds of 
+# voting leave in the amount of .45; and an increase in the log odds of voting remain in the amount
+# of 0.86.
+
+# relative risk
+exp(coef(m_partisan))
+
+# predicted probabilities
+table(compl_data$party_id)
+table(compl_data$age) # 55-64
+table(compl_data$ethnic_group) # british
+table(compl_data$social_class) # working_class
+table(compl_data$religion) # no_religion
+table(compl_data$marital_status) # married
+median(compl_data$income) # 7
+table(compl_data$education) # undergrad
+
+pred_prob_partisan <- data.frame(party_id = rep(c("cons", "labour", "lib_dem", "no_party"),2), 
+                                 know_binary = c(1,1,1,1,0,0,0,0),
+                                 gender = rep("female",8),
+                                 age = rep("55-64",8),
+                                 ethnic_group = rep("british",8),
+                                 social_class = rep("working_class",8),
+                                 religion = rep("no_religion",8),
+                                 marital_status = rep("married",8),
+                                 income = rep(7,8),
+                                 education = rep("undergrad",8))
+
+pred_prob_partisan <- cbind(pred_prob_partisan[,1:2], 
+                            predict(m_partisan, newdata = pred_prob_partisan, "probs")) %>% 
+  pivot_longer(cols = c(no_vote, leave, remain),
+               names_to = "vote",
+               values_to = "probability") %>% 
+  mutate(know_binary = factor(know_binary))
+
+pred_prob_partisan %>% 
+  ggplot() +
+  aes(x = know_binary, y = probability, group = vote, color = vote) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~party_id)
+
+# p values
+z_partisan <- summary(m_partisan)$coefficients/summary(m_partisan)$standard.errors
+p_partisan <- data.frame((1 - pnorm(abs(z_partisan), 0, 1)) * 2)
+p_partisan
+
+# Diagnostics
+r2_mcfadden(m_partisan) # 0.174
+
+# Assess colinearity. The smallest possible value of VIF is one (absence of multicollinearity). 
+# As a rule of thumb, a VIF value that exceeds 5 or 10 indicates a problematic amount of collinearity (James et al. 2014).
+car::vif(m_partisan)
 
 # Simulation with fully informed participants
 informed_df <- compl_data %>% 
@@ -70,6 +128,64 @@ m_demographic <- multinom(eu_vote ~
                           data = compl_data, 
                           Hess = TRUE,
                           weights = prop_score)
+summary(m_demographic)
+# moving from uninformed to informed is associated with a with an increase in the log odds of 
+# voting leave in the amount of .48; and an increase in the log odds of voting remain in the amount
+# of 0.90.
+
+# relative risk
+exp(coef(m_demographic))
+
+pred_prob_demographic <- data.frame(party_id = rep(c("cons", "labour", "lib_dem", "no_party"),2), 
+                                 know_binary = c(1,1,1,1,0,0,0,0),
+                                 gender = rep("female",8),
+                                 age = rep("55-64",8),
+                                 ethnic_group = rep("british",8),
+                                 social_class = rep("working_class",8),
+                                 religion = rep("no_religion",8),
+                                 marital_status = rep("married",8),
+                                 income = rep(7,8),
+                                 education = rep("undergrad",8))
+
+pred_prob_demographic <- cbind(pred_prob_demographic[,1:2], 
+                            predict(m_demographic, newdata = pred_prob_demographic, "probs")) %>% 
+  pivot_longer(cols = c(no_vote, leave, remain),
+               names_to = "vote",
+               values_to = "probability") %>% 
+  mutate(know_binary = factor(know_binary))
+
+pred_prob_demographic %>% 
+  ggplot() +
+  aes(x = know_binary, y = probability, group = vote, color = vote) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~party_id)
+
+pred_prob_partisan$model <- "partisanship"
+pred_prob_demographic$model <- "demographic"
+pred_prob_combined <- rbind(pred_prob_partisan,
+                            pred_prob_demographic)
+
+png(file="plots/pred_probs_party.png", width = 8, height = 7, units = 'in', res = 300)
+pred_prob_combined %>% 
+  ggplot() +
+  aes(x = know_binary, y = probability, group = interaction(vote, model), color = vote, linetype = model) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~party_id)
+dev.off()
+
+# p values
+z_demographic <- summary(m_demographic)$coefficients/summary(m_demographic)$standard.errors
+p_demographic <- data.frame((1 - pnorm(abs(z_demographic), 0, 1)) * 2)
+p_demographic
+
+# Diagnostics
+r2_mcfadden(m_demographic) # 0.130
+
+# Assess colinearity. The smallest possible value of VIF is one (absence of multicollinearity). 
+# As a rule of thumb, a VIF value that exceeds 5 or 10 indicates a problematic amount of collinearity (James et al. 2014).
+car::vif(m_demographic)
 
 # Simulation with fully informed participants
 sim_fullinf_demographic <- data.frame(predict(m_demographic, newdata = informed_df, "probs")) %>% 
