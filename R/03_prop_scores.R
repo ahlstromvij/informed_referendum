039a06c62b6368654362476192d64ebe415bf366
Kristoffer Ahlstrom-Vij <ahlstromvij@Kristoffers-MacBook-Pro.local>
2023-03-21 20:19:23 +0000

    Update prop score calculation

--- R/03_prop_scores.R
+++ R/03_prop_scores.R
@@ -7,10 +7,13 @@ compl_data <- readRDS("data/bes_preprocessed_scales.rds")
 
 # create binary knowledge variables
 qs <- c("0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1")
-quantile(compl_data$know, probs = seq(0, 1, 0.1)) # use 100th percentile
-q100 <- quantile(compl_data$know, 0.5) # 1.036386265
-compl_data$know_binary <- ifelse(compl_data$know >= q100, 1, 0)
-table(compl_data$know_binary)
+quantile(compl_data$know, probs = seq(0, 1, 0.1)) 
+compl_data$know_binary <- ifelse(compl_data$know > 0, 1, 0) # use > 0, for above estimated mean
+
+compl_data %>% 
+  group_by(know_binary) %>% 
+  summarise(n = n()) %>% 
+  mutate(prop = n / sum(n)) # 49% informed
 
 # create propensity score
 compl_data$prop_score <- info_prop_scores("know_binary", 
@@ -24,8 +27,6 @@ compl_data %>%
   ggplot() +
   aes(x = prop_score) +
   geom_histogram(binwidth=0.1, color = "black", fill = "salmon")
-
-table(compl_data$prop_score)
 
 # check balance
 bal_plots <- info_bal_plots(knowledge_var = "know_binary", 
